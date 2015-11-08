33594f45567d39bafae3c577b5cd6e522af6aa59
Daniele Siragusa <dnl.siragusa@gmail.com>
2015-11-08 23:43:22 +0100

    findBy and findOneBy methods

--- src/main/java/etu/upmc/fr/GenericRepository.java
+++ src/main/java/etu/upmc/fr/GenericRepository.java
@@ -1,13 +1,16 @@
 package etu.upmc.fr;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Transactional
 public class GenericRepository<T> {
@@ -15,9 +18,11 @@ public class GenericRepository<T> {
     protected EntityManager entityManager;
 
     private final Class<T> tClass;
+    private final String selectFrom;
 
     public GenericRepository(Class<T> tClass) {
         this.tClass = tClass;
+        this.selectFrom = "select t from " + tClass.getName() + " t ";
     }
 
     @Transactional
@@ -30,7 +35,7 @@ public class GenericRepository<T> {
     @Transactional
     public T findById(Long id) {
         try {
-            return entityManager.createQuery("select t from " + tClass.getName() + " t where t.id = :id", tClass)
+            return entityManager.createQuery(selectFrom + "where t.id = :id", tClass)
                     .setParameter("id", id)
                     .getSingleResult();
         }
@@ -43,11 +48,100 @@ public class GenericRepository<T> {
     @Transactional
     public List<T> findAll() {
         try {
-            return entityManager.createQuery("select t from " + tClass.getName() + " t", tClass)
+            return entityManager.createQuery(selectFrom, tClass)
                     .getResultList();
 
         } catch (PersistenceException e) {
             return new ArrayList<>();
+        }
+    }
+
+    @Transactional
+    public List<T> findBy(Map<String, Object> params) {
+        try {
+            return generateQuery(params).getResultList();
+        } catch (PersistenceException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    @Transactional
+    public T findOneBy(Map<String, Object> params) {
+        try {
+            return (T) generateQuery(params).getSingleResult();
+        } catch (PersistenceException e) {
+            return null;
+        }
+    }
+
+    public List<T> findBy(String columnName, Object value) {
+        Map<String, Object> params = new HashMap<>(1);
+        params.put(columnName, value);
+
+        return findBy(params);
+    }
+
+    public T findOneBy(String columnName, Object value) {
+        Map<String, Object> params = new HashMap<>(1);
+        params.put(columnName, value);
+
+        return findOneBy(params);
+    }
+
+    private Query generateQuery(Map<String, Object> params) {
+        StringBuilder sb = new StringBuilder(selectFrom + "where ");
+
+        int i = 1;
+        for (Map.Entry<String, Object> entry : params.entrySet()) {
+            setEquality(sb, entry.getKey(), entry.getValue());
+
+            if (i++ < params.size()) {
+                sb.append(" and ");
+            }
+        }
+
+        Query query = entityManager.createQuery(sb.toString(), tClass);
+
+        try {
+            for (Map.Entry<String, Object> entry : params.entrySet()) {
+                setParameter(query, entry.getKey(), entry.getValue());
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+        return query;
+    }
+
+    private void setParameter(Query query, String key, Object value) throws NoSuchMethodException {
+        String getter = "get" + key.substring(0, 1).toUpperCase() + key.substring(1);
+        Method m = tClass.getMethod(getter);
+
+        if (value.getClass().isArray()) {
+            int i = 0;
+            for (Object o : (Object[])value) {
+                query.setParameter(key + i++, m.getReturnType().cast(o));
+            }
+        }
+        else {
+            query.setParameter(key, m.getReturnType().cast(value));
+        }
+    }
+
+    private void setEquality(StringBuilder sb, String key, Object value) {
+        if (value.getClass().isArray()) {
+            int length = ((Object[])value).length;
+
+            for (int i = 0; i < length; i++) {
+                sb.append("t.").append(key).append(" = :").append(key).append(i);
+
+                if (i != length - 1) {
+                    sb.append(" or ");
+                }
+            }
+        }
+        else {
+            sb.append("t.").append(key).append(" = :").append(key);
         }
     }
 
