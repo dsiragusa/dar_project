d8815dd3ef08218bd9924aa41b5a331b730a0da1
iShavgula <g.Shavgula@gmail.com>
2015-11-12 00:43:05 +0100

    Extended Service + Scheduler

--- src/main/java/etu/upmc/fr/config/ApplicationConfig.java
+++ src/main/java/etu/upmc/fr/config/ApplicationConfig.java
@@ -2,18 +2,42 @@ package etu.upmc.fr.config;
 
 import static org.springframework.context.annotation.ComponentScan.Filter;
 
+import etu.upmc.fr.entity.Account;
+import etu.upmc.fr.repository.AccountRepository;
+import etu.upmc.fr.repository.ServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 
 import etu.upmc.fr.Application;
 
+@EnableAsync
+@EnableScheduling
 @Configuration
 @PropertySource("classpath:persistence.properties")
 @ComponentScan(basePackageClasses = Application.class, excludeFilters = @Filter({Controller.class, Configuration.class}))
 @Import(MailConfig.class)
 class ApplicationConfig {
+
+	@Autowired
+	private AccountRepository accountRepository;
+
+	@Autowired
+	private ServiceRepository serviceRepository;
+
+	@Bean
+	public AccountRepository accountRepository() {
+		return accountRepository;
+	}
+
+	@Bean
+	public ServiceRepository serviceRepository() {
+		return serviceRepository;
+	}
 
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
